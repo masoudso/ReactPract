3e1c301474943fc70b5bcc781324993fc8d37073
masoudso <veisi.masoud@gmail.com>
2018-05-04 13:29:44 -0400

    using CSS conditionally for the button color

--- src/App.js
+++ src/App.js
@@ -55,7 +55,8 @@ class App extends Component {
   /************************ Render() BEGINS here ************************/
   render () {
     const style = {
-      backgroundColor : 'white',
+      backgroundColor : 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -76,6 +77,7 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red'
     }
     
     return (
