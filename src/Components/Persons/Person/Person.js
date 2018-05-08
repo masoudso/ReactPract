c2d75826371c574ffd81dabdcdaf01ac71386d86
masoudso <veisi.masoud@gmail.com>
2018-05-07 20:52:01 -0400

    t-app wrap up

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -6,8 +6,7 @@ class Person extends Component {
     render() {
         return (
             <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name}
-                    and I am {this.props.age} years old!</p>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <input type="text"
                     onChange={this.props.changed}
                     value={this.props.name} />
